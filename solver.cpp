0246f82fed1ffb15d29b40f8b38a44402f282da4
Ezaruth <34143415+Ezaruth@users.noreply.github.com>
2018-04-16 19:56:13 +0200

    alap

---
+++ solver.cpp
@@ -0,0 +1,11 @@
+#include "solver.h"
+
+#include <iostream>
+
+std::vector<std::string> solver::process(const std::vector<std::string>& infos) {
+	std::vector<std::string> commands;
+	
+	// TODO
+	
+	return commands;
+}
---
+++ solver.h
@@ -0,0 +1,12 @@
+#ifndef SOLVER_H_INCLUDED
+#define SOLVER_H_INCLUDED
+
+#include <vector>
+#include <string>
+
+class solver {
+public:
+	std::vector<std::string> process(const std::vector<std::string>& infos);
+};
+
+#endif // SOLVER_H_INCLUDED
